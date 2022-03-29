feaeecea186b39d5dc70dacd0fefb8aa57d9c24a
Lynxgsm <aili.fida.ac@gmail.com>
2022-03-29 14:34:41 +0300

    added zip test file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # package-fetcher
+
+# remove persistent menu
+
+curl https://graph.facebook.com/v2.12/me/messenger_profile?access_token=
+{
+"persistent_menu": [
+{
+"locale": "default",
+"composer_input_disabled": false
+}
+]
+}
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const bootbot = require("better-bootbot");
+const dotenv = require("dotenv");
+const env = require("env-var");
+
+dotenv.config({
+  path: ".env",
+});
+
+const config = {
+  FB_ACCESS_TOKEN: env.get("FB_ACCESS_TOKEN").required().asString(),
+  FB_VERIFY_TOKEN: env.get("FB_VERIFY_TOKEN").required().asString(),
+  FB_APP_SECRET: env.get("FB_APP_SECRET").required().asString(),
+};
 
 const bot = new bootbot({
-  accessToken: "FB_ACCESS_TOKEN",
-  verifyToken: "FB_VERIFY_TOKEN",
-  appSecret: "FB_APP_SECRET",
+  accessToken: config.FB_ACCESS_TOKEN,
+  verifyToken: config.FB_VERIFY_TOKEN,
+  appSecret: config.FB_APP_SECRET,
 });
+
+bot.setPersistentMenu([], false);
 
 bot.on("message", (payload, chat) => {
   const text = payload.message.text;
-  chat.say(`Echo: ${text}`);
+  chat.say({
+    attachment: "image",
+    url: "http://example.com/image.png",
+  });
+});
+
+bot.on("postback", (event, chat) => {
+  console.log(event, chat);
+});
+bot.on("data", (event, chat) => {
+  console.log(event, chat);
+});
+
+bot.app.get("/", (req, res) => {
+  res.send({
+    hello: "world",
+  });
 });
 
 bot.start();
